104e606f80d58038f6e4055e968991cfee82edec
Lais Alencar <laisalencar88@gmail.com>
2016-11-12 15:51:33 -0300

    Adcionar imagens e mudanca de cor

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -4,5 +4,7 @@ class Produto < ApplicationRecord
   validates :nome, uniqueness: true
 
   has_attached_file :foto, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :foto, content_type: /\Aimage\/.*\z/
+
 
 end
